574b18ddc49a82c29b319c085ee56ac150ffc0e6
Ilim001 <ilim.iskenderov@gmail.com>
2023-04-15 13:17:21 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+
+// 2
+// function num() {
+//     setTimeout(function shee(){
+//         console.log(2+2);
+//     },500)
+//     console.log('2+2');
+//     // console.log(2);
+
+// }
+// num()
+
+
+// 1
+// function num() {
+//     setTimeout(function(){
+//         console.log('10');
+//     },300)
+// }
+// console.log('1');
+
+// num()
+
+
+// 3
+// let nums = [100,2,32,44,55]
+
+
+// function leatNum() {
+//     setTimeout(function(){
+//         console.log(2);
+//     },200)
+// }
+
+// leatNum()
+
+// 2sposob
+// function nums() {
+//     setTimeout(function(){
+//         console.log(1);
+//     },1000)
+//     console.log([100,1,22,33,4]);
+// }
+// nums()
+
+
+
+// 4
+// function num() {
+//     setTimeout(function(){
+//         console.log(50+50);
+//     },1500)
+//     console.log('50+50=');
+// }
+// num()
+
+
